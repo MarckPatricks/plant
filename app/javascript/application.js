27844e38f97def090be45691708fe1d9a50d27bc
MarckPatricks <lostcauzedna@hotmail.com>
2025-03-25 12:30:26 -0600

    commit 008: scroll up

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,6 +20,25 @@ navLink.forEach(link => {
     })
 })
 
+//SHOW SCROLL UP
+
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+
+    if (window.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2");
+        scrollUpBtn.classList.remove("bottom-4");
+    }
+};
+
+window.addEventListener("scroll", scrollUp);
+
+
+//CHANGE BACKGROUND HEADER
+
 // SWIPER
 
 const swiper = new Swiper('.swiper', {
